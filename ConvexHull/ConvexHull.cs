48c1af420fd88777dfd0538d4a8f8e863a87a4d5
ayesha Mushtaque <AyeshaMushtaque@outlook.com>
2019-08-27 17:37:20 +0500

    Add project files.

---
+++ ConvexHull/ConvexHull.cs
@@ -0,0 +1,99 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConvexHull
+{
+    public sealed class ConvexHull
+    {
+
+        // Returns a new list of points representing the convex hull of
+        // the given set of points. The convex hull excludes collinear points.
+        // This algorithm runs in O(n log n) time.
+        public static IList<Point> MakeHull(IList<Point> points)
+        {
+            List<Point> newPoints = new List<Point>(points);
+            newPoints.Sort();
+            //List<Point> HullPoints= MakeHullPresorted(newPoints);
+            return MakeHullPresorted(newPoints);
+        }
+
+
+        // Returns the convex hull, assuming that each points[i] <= points[i + 1]. Runs in O(n) time.
+        public static IList<Point> MakeHullPresorted(IList<Point> points)
+        {
+            if (points.Count <= 1)
+                return new List<Point>(points);
+
+            List<Point> upperHull = new List<Point>();
+            foreach (Point p in points)
+            {
+                while (upperHull.Count >= 2)
+                {
+                    Point q = upperHull[upperHull.Count - 1];
+                    Point r = upperHull[upperHull.Count - 2];
+                    if ((q.x - r.x) * (p.y - r.y) >= (q.y - r.y) * (p.x - r.x))
+                        upperHull.RemoveAt(upperHull.Count - 1);
+                    else
+                        break;
+                }
+                upperHull.Add(p);
+            }
+            upperHull.RemoveAt(upperHull.Count - 1);
+
+            IList<Point> lowerHull = new List<Point>();
+            for (int i = points.Count - 1; i >= 0; i--)
+            {
+                Point p = points[i];
+                while (lowerHull.Count >= 2)
+                {
+                    Point q = lowerHull[lowerHull.Count - 1];
+                    Point r = lowerHull[lowerHull.Count - 2];
+                    if ((q.x - r.x) * (p.y - r.y) >= (q.y - r.y) * (p.x - r.x))
+                        lowerHull.RemoveAt(lowerHull.Count - 1);
+                    else
+                        break;
+                }
+                lowerHull.Add(p);
+            }
+            lowerHull.RemoveAt(lowerHull.Count - 1);
+
+            if (!(upperHull.Count == 1 && Enumerable.SequenceEqual(upperHull, lowerHull)))
+                upperHull.AddRange(lowerHull);
+            return upperHull;
+        }
+
+    }
+
+
+
+    public struct Point : IComparable<Point>
+    {
+
+        public double x;
+        public double y;
+
+
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+
+        public int CompareTo(Point other)
+        {
+            if (x < other.x)
+                return -1;
+            else if (x > other.x)
+                return +1;
+            else if (y < other.y)
+                return -1;
+            else if (y > other.y)
+                return +1;
+            else
+                return 0;
+        }
+
+    }
+}
---
+++ ConvexHull/ConvexHullTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace ConvexHull
+{
+    public sealed class ConvexHullTest
+    {
+        public static void Main(string[] args)
+        {
+            String line;
+            IList<Point> points= new List<Point>();
+            Console.WriteLine("--------Convex Hull Program-------");
+            Console.WriteLine("----Reading the file from input.txt----");
+            StreamReader sr = new StreamReader(@"input.txt");
+            line = sr.ReadLine();
+            while (line != null)
+            {
+                //write the line to console window
+                Console.WriteLine(line);
+                var point = line.Split(' ');
+                double x = Convert.ToDouble(point[0]);
+                double y = Convert.ToDouble(point[1]);
+                Point p = new Point(x, y);
+                points.Add(p);
+                line = sr.ReadLine();
+            }
+            sr.Close();
+           
+            IList<Point> ConvexHullPoints = ConvexHull.MakeHull(points);
+            Console.WriteLine("--------The points that belongs to Convex hull-------");
+            //Writing output to the output.txt
+            StreamWriter sw = new StreamWriter(@"output.txt");
+            foreach (var item in ConvexHullPoints)
+            {
+                sw.WriteLine(item.x + " " + item.y);
+                Console.WriteLine(item.x + " " + item.y);
+            }
+            sw.Close();
+            Console.WriteLine("--------Convex Hull-------");
+            Console.ReadLine();
+        }
+    }
+}
